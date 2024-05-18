8f8951694e3acd11676611ad39158c17e1d0f471
Ethan-Chiu <ethanchiu0608@gmail.com>
2024-05-18 18:44:47 +0800

    Run hmipt demo

--- test/hmipt_run.py
+++ test/hmipt_run.py
@@ -2,6 +2,8 @@ import logging
 import torch
 from hmipt.utils.config import get_config_from_json
 from hmipt.src.models.hmipt import HmipT
+import numpy as np
+import torch.nn as nn
 # from hmipt import HmipT
 # from hmipt import process_config
 
@@ -12,6 +14,25 @@ logger = logging.getLogger()
 
 model = HmipT(config=config, logger=logger) 
 
-checkpoint_path = "./checkpoint_fianl.pth.tar"
+checkpoint_path = "./checkpoint_7.pth.tar"
 checkpoint = torch.load(checkpoint_path)
 model.load_state_dict(checkpoint['state_dict'])
+model = model.cuda()
+
+
+# _, proto = yolo(imgs_input)[:2]
+# proto: np.ndarray = proto[-1]
+proto = torch.randn(5, 32, 60, 48).cuda()
+hand = torch.randn(1, 5, 24, 14).cuda()
+head = torch.randn(1, 5, 7).cuda()
+
+
+pooling_layer = nn.AvgPool2d(kernel_size=2).cuda()
+pooled_proto: np.ndarray = pooling_layer(proto)
+pooled_proto = pooled_proto.unsqueeze(dim=0)
+
+output = model(pooled_proto, hand, head)
+output = output.squeeze()
+
+print(output)
+print(len(output))
