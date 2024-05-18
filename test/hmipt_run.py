a65eee3846d37217564f9e0c22197c818d533ec9
Ethan-Chiu <ethanchiu0608@gmail.com>
2024-05-18 18:49:11 +0800

    Add instructions

--- test/hmipt_run.py
+++ test/hmipt_run.py
@@ -1,24 +1,32 @@
 import logging
 import torch
-from hmipt.utils.config import get_config_from_json
-from hmipt.src.models.hmipt import HmipT
 import numpy as np
 import torch.nn as nn
-# from hmipt import HmipT
-# from hmipt import process_config
+from hmipt.utils.config import get_config_from_json
+from hmipt.src.models.hmipt import HmipT
 
+
+# NOTE: 
 config_path = "./hmipt.json"
+checkpoint_path = "./checkpoint_7.pth.tar"
+
+# -------------------------------------------------------------------
+# Set up model 
+# -------------------------------------------------------------------
 config, _ = get_config_from_json(config_path)
 
 logger = logging.getLogger()
 
 model = HmipT(config=config, logger=logger) 
 
-checkpoint_path = "./checkpoint_7.pth.tar"
 checkpoint = torch.load(checkpoint_path)
 model.load_state_dict(checkpoint['state_dict'])
 model = model.cuda()
 
+
+# -------------------------------------------------------------------
+# NOTE: Put real data here 
+# -------------------------------------------------------------------
 
 # _, proto = yolo(imgs_input)[:2]
 # proto: np.ndarray = proto[-1]
@@ -26,6 +34,10 @@ proto = torch.randn(5, 32, 60, 48).cuda()
 hand = torch.randn(1, 5, 24, 14).cuda()
 head = torch.randn(1, 5, 7).cuda()
 
+
+# -------------------------------------------------------------------
+# Inference 
+# -------------------------------------------------------------------
 
 pooling_layer = nn.AvgPool2d(kernel_size=2).cuda()
 pooled_proto: np.ndarray = pooling_layer(proto)
